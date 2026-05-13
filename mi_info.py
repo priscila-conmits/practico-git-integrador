882bd44c18d96b90f66a176fbe92502705bc8de9
priscila-conmits <dominguezpriscila2005@gmail.com>
2026-05-12 21:02:09 -0300

    Add personal information script mi_info.py
    git add mi_info.py
    git commit -m "Agregar script con informacion personal"

---
+++ mi_info.py
@@ -0,0 +1,7 @@
+# mi_info.py - Informacion personal
+nombre = "Priscila"
+edad = 20
+materia = "Software Factory"
+print(f"Hola, soy {nombre}")
+print(f"Tengo {edad} años")
+print(f"Estoy cursando {materia}")
